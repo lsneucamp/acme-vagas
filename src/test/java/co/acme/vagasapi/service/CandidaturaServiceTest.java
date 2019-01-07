6cb7bbe5f3599c7a0a2f47e4a0ed2df471ae4a6a
Luciano Neucamp <luciano.neucamp@inmetrics.com.br>
2019-01-06 23:11:25 -0200

    first commit

---
+++ src/test/java/co/acme/vagasapi/service/CandidaturaServiceTest.java
@@ -0,0 +1,12 @@
+package co.acme.vagasapi.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CandidaturaServiceTest {
+
+    @Test
+    public void onBeforeSave() {
+    }
+}
---
+++ src/test/java/co/acme/vagasapi/service/DistanciaServiceTest.java
@@ -0,0 +1,12 @@
+package co.acme.vagasapi.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DistanciaServiceTest {
+
+    @Test
+    public void calcularPontuacaoDistancia() {
+    }
+}
